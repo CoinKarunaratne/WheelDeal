d59e56fc74d89cbf6a3eb0f839cd266ee34615b6
A Karunaratne <coinkarunaratne@gmail.com>
2023-02-16 19:17:40 +1300

    backend: installed npm, setup auth routers, authenticated & authorize

---
+++ server/controllers/auth.js
@@ -0,0 +1,51 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const register = async (req, res) => {
+  try {
+    const { firstName, lastname, email, password, picturePath, suburb, city } =
+      req.body;
+
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastname,
+      email,
+      password: hash,
+      picturePath,
+      suburb,
+      city,
+      viewedProfile: Math.floor(Math.random() * 1000),
+      numberOfSales: Math.floor(Math.random() * 100),
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      res.status(400).json({ error: "User doesn't exist." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(400).json({ error: "Password is incorrect." });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 50,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    suburb: String,
+    city: String,
+    viewedProfile: Number,
+    numberOfSales: Number,
+    joinedDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
